0424379fd499715cde3a163f8823a8ba8a15b69d
Taiyu Yoshizawa <taiyu31@gmail.com>
2020-12-07 22:00:28 +0900

    react-map-gl Test

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, {DocumentContext, Head, Html, Main, NextScript} from 'next/document'
+
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return {...initialProps}
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <style>{`html,body { margin: 0; padding: 0; }`}</style>
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
 import { NextPage } from "next";
-import Test from "~/components/Test";
+import ReactMapGL from 'react-map-gl';
+import {useState} from "react";
 
-const Page: NextPage = () => <Test />;
+const Page: NextPage = () => {
+    const [viewport, setViewport] = useState({
+        width: 400,
+        height: 400,
+        latitude: 35.681236,
+        longitude: 139.767125,
+        zoom: 10
+    });
+
+    return (
+        <ReactMapGL
+            {...viewport}
+            width={"100vw"}
+            height={"100vh"}
+            mapStyle={'https://api.maptiler.com/maps/jp-mierune-dark/style.json?key=' + process.env.MAPTILER_ACCESS_TOKEN}
+            mapboxApiAccessToken={process.env.MAPBOX_ACCESSS_TOKEN}
+            onViewportChange={nextViewport => setViewport(nextViewport)}
+        />
+    )
+}
 export default Page;
