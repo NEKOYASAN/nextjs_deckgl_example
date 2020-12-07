9ca43a6b4ef68ae57f6be46357208659af02fb72
Nekoya3 <taiyu31@gmail.com>
2020-12-08 00:15:37 +0900

    Add DeckGL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import { NextPage } from "next";
-import ReactMapGL from 'react-map-gl';
+import {StaticMap} from 'react-map-gl';
 import {useState} from "react";
+import {GeoJsonLayer} from "@deck.gl/layers";
+import {DeckGL} from "deck.gl";
 
 const Page: NextPage = () => {
+    const data = require('~/datas/data.json')
     const [viewport, setViewport] = useState({
         width: 400,
         height: 400,
@@ -11,15 +14,37 @@ const Page: NextPage = () => {
         zoom: 10
     });
 
+    const layer = new GeoJsonLayer({
+        data,
+        filled: true,
+        stroked: true,
+        getLineWidth: 10,
+        getLineColor: [255, 0, 0],
+        getFillColor: () => {
+            const rand = Math.floor(Math.random() * Math.floor(10))
+            if (rand <= 5) {
+                return [0, 0, 255]
+            }
+            return [255, 255, 255, 50]
+        }
+    })
+
     return (
-        <ReactMapGL
-            {...viewport}
+        <DeckGL
             width={"100vw"}
             height={"100vh"}
-            mapStyle={'https://api.maptiler.com/maps/jp-mierune-dark/style.json?key=' + process.env.MAPTILER_ACCESS_TOKEN}
-            mapboxApiAccessToken={process.env.MAPBOX_ACCESSS_TOKEN}
-            onViewportChange={nextViewport => setViewport(nextViewport)}
-        />
+            controller
+            layers={[layer]}
+            viewState={viewport}
+            onViewStateChange={(viewState) => setViewport(viewState.viewState)}
+        >
+            <StaticMap
+                width={100}
+                height={100}
+                mapStyle={'https://api.maptiler.com/maps/jp-mierune-dark/style.json?key=' + process.env.MAPTILER_ACCESS_TOKEN}
+                mapboxApiAccessToken={process.env.MAPBOX_ACCESSS_TOKEN}
+            />
+        </DeckGL>
     )
 }
 export default Page;
